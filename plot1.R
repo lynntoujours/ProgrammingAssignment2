6c5618d131e8f35d5049932444bafe90171fb960
dongfan1995@163.com <dongfan1995@163.com>
2014-06-08 20:28:25 +0200

    Exploratory Data Analysis Project 1

---
+++ plot1.R
@@ -0,0 +1,6 @@
+rawdata<-read.table("household_power_consumption.txt", header = TRUE, nrows=100000, sep=";")
+data<-subset(rawdata, Date=="1/2/2007"| Date == "2/2/2007")
+data<-transform(data, Global_active_power=as.numeric(as.character(Global_active_power)))
+png(file="plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power(kilowatts)", col="red")
+dev.off()
